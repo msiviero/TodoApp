da969cb180822847d0953fe39a5926b26b4d32f1
Marco S <marco.siviero@doctolib.com>
2025-04-01 15:58:41 +0200

    Added default server listen url

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -2,9 +2,9 @@ using TodoApp.Services;
 using Microsoft.EntityFrameworkCore;
 
 var devCorsPolicyName = "_dev";
+var port = 5000;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(devCorsPolicyName, policy => policy.AllowAnyOrigin().AllowAnyHeader());
@@ -27,4 +27,4 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapControllers();
-app.Run();
+app.Run($"http://0.0.0.0:{port}");
